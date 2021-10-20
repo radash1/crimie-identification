7c43efa0c8ae9c64a3d1a74e9bfc9b6e8f09d3e9
Shakir Samoon <shakirsamoon@gmail.com>
2021-10-20 22:35:51 +0530

    update

--- new/app/src/components/SearchItem.jsx
+++ new/app/src/components/SearchItem.jsx
@@ -14,7 +14,7 @@ const SearchItem = ({ data }) => {
   return (
     <div className={styles.item} onClick={onClick}>
       <p className={styles.name}>{data.name}</p>
-      <p className={styles.age}>Age: {data.age}</p>
+      <p className={styles.age}>Age: {data.suspect_age}</p>
     </div>
   );
 };
